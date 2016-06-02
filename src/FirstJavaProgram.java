1269c3bc0a46b8ce91f6b44f4082cd611c290577
kinhak <kapilkinha@gmail.com>
2016-06-02 21:33:32 +0800

    modified java file to add git comment

--- src/FirstJavaProgram.java
+++ src/FirstJavaProgram.java
@@ -3,7 +3,7 @@ public class FirstJavaProgram {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("My name is kapil kinha");
+		System.out.println("My name is kapil kinha and i am using git");
 	}
 
 }
