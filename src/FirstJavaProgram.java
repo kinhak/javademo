36d8c24648a088017b5387e8d87979cbe6b7298b
kinhak <kapilkinha@gmail.com>
2016-06-06 21:43:44 +0800

    Added arraylist code

--- src/FirstJavaProgram.java
+++ src/FirstJavaProgram.java
@@ -1,9 +1,21 @@
+import java.util.*;
 
 public class FirstJavaProgram {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("My name is kapil kinha and i am using git");
+		ArrayList<String> kap = new ArrayList<String>();
+		kap.add("kapil");
+		kap.add("ajay");
+		kap.add("kapil");
+		kap.add("soham");
+		
+		Iterator i = kap.listIterator();
+		while(i.hasNext()){
+			System.out.println(i.next());
+		}
+		
 	}
 
 }
